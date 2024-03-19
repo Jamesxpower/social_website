e3061b68badd510393a53b215c46a6fa3f278a41
jamesxpower <jamesxpower@gmail.com>
2024-03-19 17:45:02 +0800

    3/19 study first commit

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 def user_login(request):
     if request.method == "POST":
@@ -29,3 +29,20 @@ def dashboard(reuest):
     return render(request=reuest,
                   template_name='account/dashboard.html',
                   context={'section': 'dashboard'})
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(request=request, template_name='account/register_done.html',
+                          context={"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request=request, template_name="account/register.html",
+                  context={'user_form': user_form})
