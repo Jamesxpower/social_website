604ca5c8feb0e2c2097181fcb541cf79a3de9400
jamesxpower <jamesxpower@gmail.com>
2024-03-27 17:05:59 +0800

    3/27 study commit

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from .secret import secret_info
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -161,4 +162,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 RUNSERVERPLUS_SERVER_ADDRESS_PORT = '0.0.0.0:8000'
 
-
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
